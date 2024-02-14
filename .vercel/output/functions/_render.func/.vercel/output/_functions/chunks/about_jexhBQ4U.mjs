2fd7afdc4ba774967a94b0a87038a35854a4348b
進藤雄太朗 <nihonshishinyu1994@gmail.com>
2024-02-14 23:37:43 +0900

    fix:不要な読み込み削除

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/about_jexhBQ4U.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./prerender_WP998Qi3.mjs').then(n => n.b);
+
+export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -2,7 +2,7 @@ import 'node:fs';
 import { e as appendForwardSlash, j as joinPaths, t as trimSlashes, s as slash, p as prependForwardSlash, r as removeTrailingForwardSlash, f as collapseDuplicateSlashes } from './chunks/astro/assets-service_PBjL_1Xw.mjs';
 import { serialize, parse } from 'cookie';
 import { A as AstroError, R as ResponseSentError, l as MiddlewareNoDataOrNextCalled, n as MiddlewareNotAResponse, G as GetStaticPathsRequired, o as InvalidGetStaticPathsReturn, p as InvalidGetStaticPathsEntry, q as GetStaticPathsExpectedParams, t as GetStaticPathsInvalidRouteParam, P as PageNumberParamNotFound, N as NoMatchingStaticPathFound, v as PrerenderDynamicEndpointPathCollide, w as LocalsNotAnObject, x as ASTRO_VERSION, C as ClientAddressNotAvailable, S as StaticClientAddressNotAvailable, y as renderEndpoint, z as ReservedSlotName, B as renderSlotToString, D as renderJSX, H as chunkToString, J as CantRenderPage, K as renderPage$1 } from './chunks/astro_HsTSqLAI.mjs';
-import { l as levels, g as getEventPrefix, L as Logger, A as AstroIntegrationLogger, manifest } from './manifest_oBLNywn6.mjs';
+import { l as levels, g as getEventPrefix, L as Logger, A as AstroIntegrationLogger, manifest } from './manifest_erdCrwQO.mjs';
 import 'kleur/colors';
 import 'clsx';
 import buffer from 'node:buffer';
@@ -2123,10 +2123,10 @@ const adapter = /*#__PURE__*/Object.freeze(/*#__PURE__*/Object.defineProperty({
 }, Symbol.toStringTag, { value: 'Module' }));
 
 const _page0  = () => import('./chunks/generic_5edSlyis.mjs');
-const _page1  = () => import('./chunks/index_4sEPj_cr.mjs');
-const _page2  = () => import('./chunks/_.._IhKuPCsE.mjs');
-const _page3  = () => import('./chunks/index_HzUliC4G.mjs');
-const _page4  = () => import('./chunks/about_xolr9lw7.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/endpoint/generic.js", _page0],["src/pages/article/index.astro", _page1],["src/pages/article/[...slug].astro", _page2],["src/pages/index.astro", _page3],["src/pages/about.astro", _page4]]);
+const _page1  = () => import('./chunks/index_fjTAcS4s.mjs');
+const _page2  = () => import('./chunks/_.._EbbMKnj0.mjs');
+const _page3  = () => import('./chunks/index_ADhsSFbq.mjs');
+const _page4  = () => import('./chunks/about_jexhBQ4U.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/endpoint/generic.js", _page0],["src/pages/article/index.astro", _page1],["src/pages/article/[...slug].astro", _page2],["src/pages/index.astro", _page3],["src/pages/about.astro", _page4]]);
 const _manifest = Object.assign(manifest, {
 	pageMap,
 	renderers,
