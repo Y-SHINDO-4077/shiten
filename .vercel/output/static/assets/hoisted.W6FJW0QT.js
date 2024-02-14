89b03200b2238a33f58a1fb8f85ce6386aca03e4
進藤雄太朗 <nihonshishinyu1994@gmail.com>
2024-02-14 23:31:04 +0900

    fix:MVレスポンシブ調整、title description調整

--- .vercel/output/static/assets/hoisted.W6FJW0QT.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.3k8mKLFi.js";const o=document.querySelectorAll(".js-tooltip");o.forEach(t=>{t.addEventListener("click",()=>{t.classList.toggle("is-active")})});
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = 'SHITEN';
 export const SITE_DESCRIPTION = 'SHITEN いままで知らなかった、まちの視点';
