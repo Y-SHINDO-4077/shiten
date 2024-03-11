20a158c9333f732b7aca2bb7483f706a918ff182
進藤雄太朗 <nihonshishinyu1994@gmail.com>
2024-03-11 17:44:09 +0900

    fix:ハンバーガーないの高さ調整

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/about_-OKGK6Yk.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./prerender_MdtPhu1J.mjs').then(n => n.b);
-
-export { page };
---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index_VPtujDUT.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./prerender_CbPFsUgA.mjs').then(n => n.a);
+
+export { page };
